6ca17b61917aa9397302cc338d4ad8d2731de6cc
Sheikh Mohd Aamir <129277096+SheikhMohdAamir@users.noreply.github.com>
2023-09-11 19:02:27 +0530

    installed express and trying middleware

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app=express()
+app.use((req,res,next)=>{
+    console.log('In a middleware')
+    next()
+})
+app.use((req,res,next)=>{
+    console.log('In another middleware')
+    res.send('<h1>Hello World</h1>')
+})
+app.listen(3000)
