0dc81df9abbd5f0ba3b4e6985116ca3034778597
Sheikh Mohd Aamir <129277096+SheikhMohdAamir@users.noreply.github.com>
2023-09-13 16:58:47 +0530

    Assignment 3

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express=require('express')
 const bodyParser=require('body-parser')
 const app=express()
-
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
-app.use('/product-page',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><input type="number" name="size"/><button type="submit">Submit</button></form>')
+app.use('/admin',adminRoutes)
+app.use('/shop',shopRoutes)
+app.use('/',(req, res, next)=>{
+    res.send('<h1>HOME PAGE</h1>')
 })
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello World</h1>')
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 app.listen(3000)
