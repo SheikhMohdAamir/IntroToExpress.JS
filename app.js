2679acef64a03be7dd80abbe6cfc12373d768751
Sheikh Mohd Aamir <129277096+SheikhMohdAamir@users.noreply.github.com>
2023-09-13 15:22:27 +0530

    Assignment 2

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-
+const express=require('express')
+const bodyParser=require('body-parser')
 const app=express()
-app.use((req,res,next)=>{
-    console.log('In a middleware')
-    next()
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/product-page',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><input type="number" name="size"/><button type="submit">Submit</button></form>')
 })
-app.use((req,res,next)=>{
-    console.log('In another middleware')
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello World</h1>')
 })
 app.listen(3000)
